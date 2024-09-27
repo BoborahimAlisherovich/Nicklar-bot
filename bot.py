8da573ea6e3d371c4d59796e31c0e4e85a689e7d
Boborahim <boborahimrustamqulov0@gmail.com>
2024-09-27 20:50:41 +0500

    Nick bot

--- bot.py
+++ bot.py
@@ -45,12 +45,13 @@ async def start_command(message: Message,state:FSMContext):
     try:
         db.add_user(full_name=full_name, telegram_id=telegram_id)
         await message.answer(text=f"Salom! {full_name} Botimizga xush kelibsiz! 🎉 Siz o'ziga xos, qiziqarli va esda qolarli niklar yaratmoqchimisiz? Endi bu juda oson! 💫 Shunchaki ism kiriting va biz sizga eng zo'r variantlarni taqdim etamiz. ",reply_markup=admin_keyboard.start_button)
+        await state.clear()
 
 
 
     except:
         await message.answer(f"Salom! {full_name} Botimizga xush kelibsiz! 🎉 Siz o'ziga xos, qiziqarli va esda qolarli niklar yaratmoqchimisiz? Endi bu juda oson! 💫 Shunchaki ism kiriting va biz sizga eng zo'r variantlarni taqdim etamiz. 🚀",reply_markup=admin_keyboard.start_button)
-
+        await state.clear()
                              
 # Kanalga obuna bo'lishni tekshiruvchi handler
 @dp.message(IsCheckSubChannels())
